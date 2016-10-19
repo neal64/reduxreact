aaa610eaa48b5cb9fc638d3cebee6bc8e4a68eb7
Nilesh <nealpatel64@gmail.com>
2016-10-18 19:28:47 -0700

    change video list props

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,6 +1,6 @@
 import  React from "react";
 import VideoListItem from "./video_list_item";
-
+// Select Video List
 const VideoList = (props) => {
     const videoItems = props.videos.map((video) =>{
         return (
